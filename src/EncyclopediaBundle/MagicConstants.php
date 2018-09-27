12eafadfe05e81a6da5d83b2410133a0ed20a5d8
mirandaguillaume <guillaume11miranda@gmail.com>
2018-09-27 10:04:25 +0000

    Apply fixes from StyleCI

--- src/EncyclopediaBundle/MagicConstants.php
+++ src/EncyclopediaBundle/MagicConstants.php
@@ -11,19 +11,33 @@ namespace EncyclopediaBundle;
 class MagicConstants
 {
     const NORMAL = 'NORMAL';
+
     const RITUAL = 'RITUAL';
+
     const WINTER = 'WINTER';
+
     const FALL = 'FALL';
+
     const SUMMER = 'SUMMER';
+
     const SPRING = 'SPRING';
+
     const INCANTATION = 'INCANTATION';
+
     const LOW_LEVEL = 'LOW';
+
     const MID_LEVEL = 'MID';
+
     const HIGH_LEVEL = 'HIGH';
+
     const TOUCH = 'TOUCH';
+
     const SPELLCASTER = 'SPELLCASTER';
+
     const CLOSE = 'CLOSE';
+
     const FAR = 'FAR';
+
     const ILLIMITED = 'ILLIMITED';
 
     const MAGIC_CASTING_TYPE = [
